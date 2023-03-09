4c45db9c9f02494927f2abfaed02edf9ede8ba53
michal <michallehwark511@gmail.com>
2023-03-09 15:55:33 +0100

    End of section 2 of Udemy course

---
+++ API/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace API;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+// question mark next to string'?' means taht its optional
+    public string Summary { get; set; }
+}
