c471433ce381274ae82cef7fab8ca7e0410d519a
michal <michallehwark511@gmail.com>
2023-03-27 15:47:32 +0200

    End of section 9

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -10,7 +10,7 @@ import {v4 as uuid} from 'uuid';
 export default observer(function ActivityForm() { //:selectedActivity is changing the name of the activity vairable passed from Props
 
     const {activityStore} = useStore();
-    const {selectedActivity, createActivity, updateActivity, loading, loadActivity, loadingInitial} = activityStore;
+    const {createActivity, updateActivity, loading, loadActivity, loadingInitial} = activityStore;
     const {id} = useParams();
     const navigate = useNavigate();
 
